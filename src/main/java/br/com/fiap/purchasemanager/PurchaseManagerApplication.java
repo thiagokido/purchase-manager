338b39e43bb80ddd48b98d6eee189561c5685237
Thiago Kido <thiagokido@MacBook-Air-de-Thiago-2.local>
2023-10-23 19:44:20 -0300

    Starting project

---
+++ src/main/java/br/com/fiap/purchasemanager/PurchaseManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.purchasemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/purchasemanager/PurchaseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.purchasemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
